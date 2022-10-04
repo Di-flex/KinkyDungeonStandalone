4534e5bc029d0b2c6fbe8edef5c45b88bf33448e
Ada <gubertpol0@gmail.com>
2022-10-04 00:20:46 -0700

    notify

--- src/loader.js
+++ src/loader.js
@@ -15,7 +15,7 @@ const PROGRESS_TEXT = document.getElementById('load-progress-text');
 function updateProgress(text, num, div) {
     const pct = 50 * num / div;
     PROGRESS_BAR.style.width = `${pct}%`; 
-    PROGRESS_TEXT.innerText = `${text} ${num} / ${div}`;
+    PROGRESS_TEXT.innerText = `${text} ${num} / ${div} - if not working, try https://ada18980.github.io/KinkiestDungeon/`;
 }
 
 
